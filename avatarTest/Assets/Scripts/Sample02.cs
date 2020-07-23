eb261dbb1fb145cd994a07d5166e3297fe21b75c
ghkdlxld <ohsg97@gmail.com>
2020-07-23 17:01:43 +0900

    avatar unityfile
    avatar

---
+++ avatarTest/Assets/Scripts/Sample02.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample02 : MonoBehaviour
+{
+    public GameObject humanoid;
+    public bool mirror = true;
+    public bool move = true;
+
+    NtUnity.Kinect nt;
+    NtUnity.HumanoidSkeleton hs;
+
+    void Start()
+    {
+        nt = new NtUnity.Kinect();
+        hs = new NtUnity.HumanoidSkeleton(humanoid);
+    }
+
+    void Update()
+    {
+        nt.setRGB();
+        nt.setSkeleton();
+        nt.setFace();
+        nt.imshowBlack();
+        int n = nt.getSkeleton();
+        if (n > 0)
+        {
+            hs.set(nt, 0, mirror, move);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        nt.stopKinect();
+    }
+}
